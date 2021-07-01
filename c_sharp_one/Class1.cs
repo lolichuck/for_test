0ed0ab4fd6b714a458e029f488b334debcad56a1
Vlad Shaplin <shaplinvlad@live.com>
2021-07-01 13:33:26 +0300

    Initial commit

---
+++ c_sharp_one/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+using System.Diagnostics;
+
+namespace c_sharp_one
+{
+    public class ForTest
+    {
+        public string GetSolution()
+        {
+            string current_path = Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName);
+            return current_path;
+        }
+    }
+}
---
+++ c_sharp_two/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using c_sharp_one;
+
+namespace c_sharp_two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForTest ft = new ForTest();
+
+            Console.WriteLine(ft.GetSolution());
+            Console.ReadKey();
+        }
+    }
+}
